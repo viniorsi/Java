c58efb361526dcbf5925ae533e08e17a016f3a96
Vinicius Orsi <vinicius.orsi@autopass.com.br>
2024-07-01 10:41:09 -0300

    iniciando projeto de aplicativo de viagens

---
+++ TravelEase/src/main/java/com/viniorsi/TravelEase/Domain/User/Entity/User.java
@@ -0,0 +1,26 @@
+package com.viniorsi.TravelEase.Domain.User.Entity;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private Long id  ;
+    private String Cpf ;
+    private String name ;
+    private String tel ;
+    private String ddd ;
+    private String email ;
+    private String password ;
+    private LocalDate birthday ;
+    private LocalDate creation_date ;
+    private String zip_code ;
+    private String address ;
+    private String number ;
+    private String additional_info ;
+    private String neigborhood ;
+    private String state ;
+    private String city ;
+    private Boolean active ;
+    private int points;
+
+}
