a8b969f86302598e284466ca906b14fd90db44e8
viniorsi <orsivini@outlook.com>
2024-07-01 10:36:40 -0300

    adicionando projeto de boas praticas

---
+++ 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,69 @@
+package br.com.alura;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.service.AbrigoService;
+import br.com.alura.service.PetService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+
+
+    public static void main(String[] args) {
+        AbrigoService abrigoService = new AbrigoService();
+        PetService petService = new PetService();
+        ClientHttpConfiguration clientHttpConfiguration = new ClientHttpConfiguration();
+        AdopetConsoleApplication adopetConsoleApplication = new AdopetConsoleApplication();
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (opcaoEscolhida != 5) {
+                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+                System.out.println("1 -> Listar abrigos cadastrados");
+                System.out.println("2 -> Cadastrar novo abrigo");
+                System.out.println("3 -> Listar pets do abrigo");
+                System.out.println("4 -> Importar pets do abrigo");
+                System.out.println("5 -> Sair");
+
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                if (opcaoEscolhida == 1) {
+                    abrigoService.listarAbrigo();
+                } else if (opcaoEscolhida == 2) {
+                    abrigoService.cadastrarAbrigo();
+                } else if (opcaoEscolhida == 3) {
+                    petService.listarPets();
+                } else if (opcaoEscolhida == 4) {
+                    petService.importarPets();
+                } else if (opcaoEscolhida == 5) {
+                    break;
+                } else {
+                    System.out.println("NÚMERO INVÁLIDO!");
+                    opcaoEscolhida = 0;
+                }
+            }
+            System.out.println("Finalizando o programa...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ 3149-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/service/AbrigoService.java
@@ -0,0 +1,64 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AbrigoService {
+
+    ClientHttpConfiguration clientHttpConfiguration = new ClientHttpConfiguration();
+
+    public void listarAbrigo() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        String uri = "http://localhost:8080/abrigos";
+        HttpResponse<String> response = clientHttpConfiguration.dispararRequisiçãoGet( uri);
+        String responseBody = response.body();
+        JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
+        System.out.println("Abrigos cadastrados:");
+        for (JsonElement element : jsonArray) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            long id = jsonObject.get("id").getAsLong();
+            String nome = jsonObject.get("nome").getAsString();
+            System.out.println(id +" - " +nome);
+        }
+    }
+
+    public void cadastrarAbrigo() throws IOException, InterruptedException {
+        System.out.println("Digite o nome do abrigo:");
+        String nome = new Scanner(System.in).nextLine();
+        System.out.println("Digite o telefone do abrigo:");
+        String telefone = new Scanner(System.in).nextLine();
+        System.out.println("Digite o email do abrigo:");
+        String email = new Scanner(System.in).nextLine();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("nome", nome);
+        json.addProperty("telefone", telefone);
+        json.addProperty("email", email);
+
+        HttpClient client = HttpClient.newHttpClient();
+        String uri = "http://localhost:8080/abrigos";
+        HttpResponse<String> response = clientHttpConfiguration.dispararRequisiçãoPost(uri, json);
+        int statusCode = response.statusCode();
+        String responseBody = response.body();
+        if (statusCode == 200) {
+            System.out.println("Abrigo cadastrado com sucesso!");
+            System.out.println(responseBody);
+        } else if (statusCode == 400 || statusCode == 500) {
+            System.out.println("Erro ao cadastrar o abrigo:");
+            System.out.println(responseBody);
+        }
+    }
+
+
+
+}
